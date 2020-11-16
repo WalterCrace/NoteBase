daafbfba05e83648c849b7b454902836a9169af5
WalterCrace <walter.alejandro1247@gmail.com>
2020-11-15 20:21:43 -0600

    Enviando primer commit y probando la consola de code

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+bas=float(input("Ingrese la base del triangulo:"))
+alt=float(input("Ingrese la altura del triangulo:"))
+
+tot= bas*alt /2
+
+print("----EL RESULTADO ES----")
+print("El area del triangulo es:",tot)
